1a32246c786c0d632d4eac00f075ad6b186c6660
dalent <anqiu1987@sina.com>
2014-06-12 20:06:51 +0800

    add slick grid

--- simpleHttpServer/path_js.js
+++ simpleHttpServer/path_js.js
@@ -3,8 +3,13 @@ var path_js={
 	"/":"./index.js",
 	"/index.html":"./index.js",
 	"/script.js":"./index.js",
-	"/jquery-1.11.1.min.js":"./index.js",
+	"/jquery-2.1.1.min.js":"./index.js",
 	"/demo.txt":"./index.js",
+	"/jquery.event.drag-2.2.js":"./index.js",
+	"/slick.core.js":"./index.js",
+	"/slick.grid.js":"./index.js",
+	"/slick.grid.css":"./index.js",
+	"/examples.css":"./index.js",
 };
 
 exports.path_js=path_js;
--- simpleHttpServer/start.js
+++ simpleHttpServer/start.js
@@ -1,4 +1,5 @@
 var server=require('./httpServer.js');
+//according differnet url, call different module
 var dispatch = require('./dispatch');
 
 server.onStart("127.0.0.1", 80, dispatch.dispatch);
